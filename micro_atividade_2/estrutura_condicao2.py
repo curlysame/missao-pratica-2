eebdd4e1dd469fa196ecbdfdb8c6b1960cf7ac4d
curlysame <118316442+curlysame@users.noreply.github.com>
2024-07-25 20:42:52 -0300

    Create estrutura_condicao2.py

---
+++ micro_atividade_2/estrutura_condicao2.py
@@ -0,0 +1,7 @@
+tempoExperiencia = 1
+if tempoExperiencia < 2:
+    print('Nivel de conhecimento júnior')
+elif tempoExperiencia < 5:
+    print('Nivel de conhecimento pleno')
+else:
+    print('Nivel de conhecimento Senior')
