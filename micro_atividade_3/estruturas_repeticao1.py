c65f6904a71765aaa98b06034179b8e68fc190e9
curlysame <118316442+curlysame@users.noreply.github.com>
2024-07-25 20:43:45 -0300

    Create estruturas_repeticao1.py

---
+++ micro_atividade_3/estruturas_repeticao1.py
@@ -0,0 +1,7 @@
+entrada_idade = ''
+
+while entrada_idade != '0':
+
+    entrada_idade = input('Digite um numero qualquer ou 0 para sair: ')
+    print('Numero digitado: ', entrada_idade)
+    print('Programa encerrado.')
