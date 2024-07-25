79c662c48dba57bf12b615b72b3bbd70699c824b
curlysame <118316442+curlysame@users.noreply.github.com>
2024-07-25 20:39:48 -0300

    Create estrutura_condicao1.py

---
+++ micro_atividade_1/estrutura_condicao1.py
@@ -0,0 +1,11 @@
+#criar uma verificacao temperatura menor que 30 
+#if positivo , print text A: ´a temperatura esta amena;
+#else negativo , print text B: 'Hoje esta fazendo calor'
+
+temperatura = 31
+
+if temperatura < 30:
+    print('A temperatura esta amena')
+else:
+    print('Hoje está calor') 
+ 
