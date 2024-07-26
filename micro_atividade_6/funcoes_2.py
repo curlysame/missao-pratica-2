6989e092752f9c557f9211bda8997fd730bc814e
curlysame <118316442+curlysame@users.noreply.github.com>
2024-07-25 22:05:27 -0300

    Update funcoes_2.py

--- micro_atividade_6/funcoes_2.py
+++ micro_atividade_6/funcoes_2.py
@@ -1,8 +1,21 @@
+perfil_admin = 'Admin'
+perfil_admin_lower = 'admin'
+perfil_user = 'User'
+perfil_user_lower = 'user'
+
 def loginUsuario(perfil):
     if perfil.lower() == 'admin':
-        print('Bem vindo, Administrador')
+        print("Bem Vindo, Administrador")
     else:
-        print('Bem vindo, Usuário')
+        print("Bem Vindo, Usuário")  
+
+perfil_informado = input("Digite o seu perfil: ")
+
+loginUsuario(perfil_informado)
+
+
+
+
 
 loginUsuario('Admin')
 loginUsuario('admin')
